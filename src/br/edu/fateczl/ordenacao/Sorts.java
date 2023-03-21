82a810a0b3170b4dd71bd490d3c567cf3f01f234
luiza <luizamfelix@gmail.com>
2023-03-21 15:15:57 -0300

    Atualização 23/03/2023

--- src/br/edu/fateczl/ordenacao/Sorts.java
+++ src/br/edu/fateczl/ordenacao/Sorts.java
@@ -12,7 +12,7 @@ public class Sorts {
 	}
 
 //	BUBBLE SORT
-	public static int[] Bubble(int[] vetor) {
+	public int[] Bubble(int[] vetor) {
 		int tamanho = vetor.length;
 
 		for (int rodada = 1; rodada < tamanho; rodada++) {
@@ -30,25 +30,25 @@ public class Sorts {
 	}
 
 //	MERGE SORT
-	public static int[] Merge(int[] vetor) {
-		vetor = particao(vetor, 0, (vetor.length - 1));
+	public int[] Merge(int[] vetor) {
+		vetor = subMERGE(vetor, 0, (vetor.length - 1));
 		return vetor;
 	}
 
-	private static int[] particao(int[] vetor, int inicio, int fim) {
+	private int[] subMERGE(int[] vetor, int inicio, int fim) {
 		int meio = (inicio + fim) / 2;
 		if ( inicio == fim) {
 			return vetor; // se for igual eu obrigatoriamente retorno o vetor
 	    } else{ 
-	    	/* 1/2 */particao(vetor, inicio, meio);
-			/* 2/2 */particao(vetor, (meio + 1), fim);
-			ordenacao(vetor, inicio, meio, fim);
+	    	/* 1/2 */subMERGE(vetor, inicio, meio);
+			/* 2/2 */subMERGE(vetor, (meio + 1), fim);
+			ordeMERGE(vetor, inicio, meio, fim);
 //			vou voltando recursivamente o vetor ordenado por rodada 
 			return vetor;
 	    }	
 	}
 
-	private static void ordenacao(int[] vetor, int inicio, int meio, int fim) {
+	private void ordeMERGE(int[] vetor, int inicio, int meio, int fim) {
 		int[] aux = new int[vetor.length];
 
 //		cópia dos valores dentro do intervalo sub-vetor para o vetor auxiliar;
@@ -83,6 +83,61 @@ public class Sorts {
 			}
 		}
 
+	}
+	
+//	QUICK SORT
+	public int[] Quick(int[] vetor) {
+		vetor = subQUICK(vetor, 0, vetor.length-1);
+		return vetor;
+	}
+
+	private int[] subQUICK(int[] vetor, int inicio, int fim) {
+		if (inicio < fim) {
+			// enquanto o fim for maior que o começo, o pivô não estoura pro negativo eu sigo repartindo com base no pivô fixo determinado anteriormente preciso saber a posição onde o pivo foi fixado para depois determinar os subvetores, como o "meio" em mergesort
+			int pivofixado = pivotQUICK(vetor, inicio, fim);
+			/* subvetor da esquerda */ subQUICK(vetor, inicio, (pivofixado-1));
+			/* subvetor da direita */ subQUICK(vetor, (pivofixado + 1), fim);
+		}
+		return vetor;
+		
+	}
+
+	private int pivotQUICK(int[] vetor, int inicio, int fim) {
+		// agoritmo seguindo a lógica passada no vídeo, para a ordenação do vetor
+
+		/* ponteiro começo-fim */int red = (inicio + 1);
+		/* ponteirofim-começo */int green = fim;
+
+		// caminhada das setas
+		while (red <= green) {
+			
+			while (red <= green && vetor[red] <= vetor[inicio]) {
+				// o vermelho pode estourar o comprimento do vetor, se só houverem números menores que o pivô, fazendo com que ele se encaixe na útima posição do vetor(e se isso for executado, estourar ele). o WHILE faz com que isso se repita sem sobrecarregar a pilha de execução abrindo um while só consecutivamente
+				red++;
+			} // a incrementação dele cessa e a do verde começa
+			while (green >= red && vetor[green] > vetor[inicio]) {
+//					se não for especificado esse limite, o código estoura tbm (decrementa para além do 0)
+					green--;
+			}// parou a decrementação e achou uma variável menor ou igual, se eles não estão referenciados na mesma posição, eu devo fazer uma troca
+			if (red < green) {
+//				nesse caso eles não se cruzaram ainda, depois dessa troca eu continuo rodando
+				int aux = vetor[red];
+				vetor[red] = vetor[green];
+				vetor[green] = aux;
+				
+//				logicamente se eles ainda não se cruzaram eu sigo andando
+				red++;
+				green--;
+			}
+			}
+		
+//		troca dos valores, determinando o novo pivô (número que ocupava a casa 0)
+		int aux = vetor[inicio];
+		vetor[inicio] = vetor[green];
+		vetor[green] = aux;
+		
+
+		return green;
 	}
 
 }
